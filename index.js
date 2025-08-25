de100fb248aed8d656a9161cdcfba654cd2a6074
LieutLet <thesmartalec155@gmail.com>
2025-08-25 09:03:46 -0400

    started on a class to hold the info for each website that is tbe be blocked.

--- index.js
+++ index.js
@@ -13,3 +13,30 @@ function deleteWebsite(){
 function homePage(){
     window.location.href = "index.html";
 }
+
+function addWebsite(){
+
+}
+
+class websiteToBlock{
+    constructor(siteName, url){
+        this.siteName = siteName;
+        this.url = url;
+    }
+
+    editName(newName){
+        if (this.siteName != newName) {
+            this.siteName = newName;
+        } else {
+            window.alert( newName + " is already the name of this website.");
+        }
+    }
+
+    editUrl(newUrl){
+        if (this.url != newUrl) {
+            this.url = newUrl;
+        } else {
+            window.alert( newUrl + " is already the address for this website.");
+        }
+    }
+}
