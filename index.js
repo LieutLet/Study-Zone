eb2996b12ca5ce68118aec2056061d8f1feca770
LieutLet <thesmartalec155@gmail.com>
2025-08-24 23:50:52 -0400

    Starting stuff for app, minimal functionality added.

---
+++ index.js
@@ -0,0 +1,15 @@
+function addWebsitePage(){
+    window.location.href = "addPage.html";
+}
+
+function editWebsite(){
+
+}
+
+function deleteWebsite(){
+    
+}
+
+function homePage(){
+    window.location.href = "index.html";
+}
