111b1c8080c8737f0962c7166f17f8cdd7bc63d1
LieutLet <thesmartalec155@gmail.com>
2026-01-12 23:10:23 -0500

    continued working on div stuff

--- index.js
+++ index.js
@@ -35,13 +35,6 @@ function addWebsite(){
     let url = document.getElementById("URL").value;
 
     url = getDomainName(url);
-
-    //Check for if the website is already in the list
-    /*for (let i = 0; i < websites.length; i++) {
-        if (websites[i].getName() === siteName) {
-            window.alert(siteName + " is already in the list of blocked websites.");
-            return;
-        }*/
     
     try{
         if (webMap.has(url))  {
@@ -61,24 +54,50 @@ function addWebsite(){
     document.getElementById("URL").value = "";
     window.alert(siteName + " has been added to the list of blocked websites.");
 
-    //Add to new website to index.html
-    newWebsite.newDiv();
+    //call function to go back to home page
+    window.location.href = "index.html";
+
 }
 
 class websiteToBlock{
     constructor(siteName, url){
         this.siteName = siteName;
         this.url = url;
+        this.div = newDiv();
     }
 
     //creates an div element for the website, with the website name and url, and returns it
     newDiv(){
+        //Website container div
+        let lvl2div = document.createElement("div");
+        lvl2div.className = "website";
+
+        //website icon
+        let lvl1div1 = document.createElement("div");
+        lvl1div1.className = "item";
+        lvl1div1.id = "icon";
+
+        let lvl1img1 = document.createElement("img");
+        //favicon of the webstite, to be implemented later
+        lvl1div1.appendChild(lvl1img1);
+
+        //append website icon to website container
+        lvl2div.appendChild(lvl1div1);
+
+        //website name
+        let lvl0div1 = document.createElement("div");
+        lvl0div1.className = "item";
+        lvl0div1.id = "website-name";
+        lvl0div1.innerHTML = this.siteName;
+        lvl2div.appendChild(lvl0div1);
+
         //edit icon
         let lvl0Img1 = document.createElement("img");
         lvl0Img1.src = "/images/edit.svg";
         lvl0Img1.className = "item";
         lvl0Img1.id = "icon";
         lvl0Img1.onclick = editWebsite();
+        lvl2div.appendChild(lvl0Img1);
 
         //trash icon
         let lvl0Img2 = document.createElement("img");
@@ -86,19 +105,11 @@ class websiteToBlock{
         lvl0Img2.className = "item";
         lvl0Img2.id = "icon";
         lvl0Img2.onclick = deleteWebsite();
+        lvl2div.appendChild(lvl0Img2);
 
-        //website name
-        let lvl0div1 = document.createElement("div");
-        lvl0div1.className = "item";
-        lvl0div1.id = "website-name";
-        lvl0div1.innerHTML = this.siteName;
+        //append the website container to the main container in index.html
+        document.body.appendChild(lvl2div);
 
-        //website icon
-        let lvl1div1 = document.createElement("div");
-        lvl1div1.className = "item";
-        let lvl1div2 = document.createElement("div");
-        let lvl1div3 = document.createElement("div");
-        let lvl2 = document.createElement("div");
         return newDiv;
     }
 
